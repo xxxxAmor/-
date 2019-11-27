7430c3c9bc187ae96b8af79e95140f91f51e5b19
xxxxAmor <18847649839@163.com>
2019-11-27 20:45:51 +0800

    2019.11.28

---
+++ data/index_data.js
@@ -0,0 +1,122 @@
+
+var bannerSrc = {
+  banner: [
+    {
+      imgurl:"http://202.112.194.245/space/201811690524/02.png" ,
+      navigator: "../other/other",
+    },
+    {
+      imgurl: "http://202.112.194.245/space/201811690524/02.png",
+      navigator: "../other/other",
+    },
+    {
+      imgurl: "http://202.112.194.245/space/201811690524/02.png",
+      navigator: "../other/other",
+    },
+    {
+      imgurl: "http://202.112.194.245/space/201811690524/02.png",
+      navigator: "../other/other",
+    },
+    {
+      imgurl: "http://202.112.194.245/space/201811690524/02.png",
+      navigator: "../other/other",
+    },
+    {
+      imgurl: "http://202.112.194.245/space/201811690524/02.png",
+      navigator: "../other/other",
+    },
+    {
+      imgurl: "http://202.112.194.245/space/201811690524/02.png",
+      navigator: "../other/other",
+    }
+  ]
+}
+var navSrc = {
+  nav: [
+    {
+      imgurl: "http://edu-image.nosdn.127.net/A0ED3BAF7A2572B19FD7FF1ECF71C21C.jpg",
+      navigator: "../other/other",
+      title: "数学游戏"
+    },
+    {
+      imgurl: "http://edu-image.nosdn.127.net/24c80eb7-148d-4520-92b3-19de174561d2.jpg",
+      navigator: "../other/other",
+      title: "趣味课堂"
+    },
+    {
+      imgurl: "http://edu-image.nosdn.127.net/65ECBB4D91707E1BF49A8757F01909AC.jpg",
+      navigator: "../other/other",
+      title: "问答星球"
+    },
+    
+    
+    
+  ]
+
+}
+var adSrc = {
+  ad: [
+    {
+      imgurl: "http://imgsize.ph.126.net/?enlarge=true&imgurl=http://nos.netease.com/edu-image/9cb99e9e-d95f-4958-86cf-304de7245d33.jpg?imageView&quality=100_470x184x1x95.png",
+      navigator: "../other/other",
+    },
+    {
+      imgurl: "http://202.112.194.245/space/201811690524/02.png",
+      navigator: "../other/other",
+    }, {
+      imgurl: "http://202.112.194.245/space/201811690524/02.png",
+      navigator: "../other/other",
+    }
+  ]
+}
+
+
+var courseSrc = {
+  course_grp: [
+    {
+      navigator: "../other/other",
+      title: {
+        left: "小游戏",
+        right: "更多"
+      },
+      courses: [
+        {
+          title: "滑块拼图",
+          imgurl: "http://202.112.194.245/space/201811690524/02.png",
+          navigator: "../other/other"
+        }, {
+        
+          title: "2048",
+          imgurl: "http://202.112.194.245/space/201811690524/02.png",
+          navigator: "../other/other"
+        }, {
+          
+          title: "找不同",
+          imgurl: "http://202.112.194.245/space/201811690524/02.png",
+          navigator: "../other/other"
+        }, {
+          
+          title: "别踩白块",
+          imgurl: "http://202.112.194.245/space/201811690524/02.png",
+          navigator: "../other/other"
+        }, 
+        
+      ]
+    },
+
+
+
+
+   
+
+
+
+
+
+  ]
+}
+
+module.exports.navSrc = navSrc;
+module.exports.bannerSrc = bannerSrc;
+module.exports.adSrc = adSrc;
+module.exports.courseSrc = courseSrc;
